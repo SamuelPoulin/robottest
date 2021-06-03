7aaf350774f75a27f5afb2fc1cf4d9e6b2cd2c4c
Samuel Poulin <samuel-poulin@outlook.com>
2021-06-02 20:31:38 -0400

    Removed json for ts data

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,34 +1,35 @@
-import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
-import * as data from 'src/assets/data.json';
+import { Component, ViewChildren, QueryList } from '@angular/core';
+import data from 'src/app/models/data';
 import { CategoryPanelComponent } from './category-panel/category-panel.component';
 import { Category } from 'src/app/models/category';
 import { Level } from 'src/app/models/level';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-    categories: Category[];
-    levels: Level[];
+  categories: Category[];
+  levels: Level[];
 
-    @ViewChildren(CategoryPanelComponent) categoryPanels: QueryList<CategoryPanelComponent>;
+  @ViewChildren(CategoryPanelComponent)
+  categoryPanels: QueryList<CategoryPanelComponent>;
 
-    constructor() {
-        this.categories = data.categories;
-        this.levels = data.levels;
+  constructor() {
+    this.categories = data.categories;
+    this.levels = data.levels;
+  }
+
+  get sum(): number {
+    let sum = 0;
+
+    if (this.categoryPanels) {
+      this.categoryPanels.forEach((categoryPanel) => {
+        sum += categoryPanel.sum;
+      });
     }
 
-    get sum(): number {
-        let sum = 0;
-
-        if (this.categoryPanels) {
-            this.categoryPanels.forEach((categoryPanel) => {
-                sum += categoryPanel.sum;
-            });
-        }
-
-        return sum;
-    }
+    return sum;
+  }
 }
---
+++ src/app/models/data.ts
@@ -0,0 +1,432 @@
+export default {
+  categories: [
+    {
+      name: 'Physical',
+      color: '#c62828',
+      criterias: [
+        {
+          description: 'Have an attractive face',
+          points: 5,
+        },
+        {
+          description: 'Healthy weight for your age and sex',
+          points: 4,
+        },
+        {
+          description: 'Particularly fit or strong or muscular',
+          points: 2,
+        },
+        {
+          description: "At least 6'0 tall (M) or good figure (F)",
+          points: 3,
+        },
+        {
+          description: 'Exercise regularly',
+          points: 1,
+        },
+        {
+          description: 'Have a decently healthy diet',
+          points: 1,
+        },
+        {
+          description: 'Minor or no physical abnormalities',
+          points: 3,
+        },
+        {
+          description: 'No smoking addiction (vaping included)',
+          points: 1,
+        },
+        {
+          description: "Don't smoke weed or only occasionally",
+          points: 1,
+        },
+        {
+          description: 'No alcohol addiction',
+          points: 1,
+        },
+        {
+          description: 'No addiction to any other drug',
+          points: 2,
+        },
+        {
+          description: 'Good dental hygiene',
+          points: 1,
+        },
+        {
+          description: 'Shower regularly',
+          points: 2,
+        },
+        {
+          description: 'Have at least a decent fashion sense',
+          points: 2,
+        },
+        {
+          description: 'Visit a doctor regularly',
+          points: 1,
+        },
+        {
+          description: 'Have a good hairstyle',
+          points: 2,
+        },
+        {
+          description: 'Take pride in your body',
+          points: 1,
+        },
+      ],
+    },
+    {
+      name: 'Mental',
+      color: '#283593',
+      criterias: [
+        {
+          description: 'No significant autism, aspergers, etc.',
+          points: 3,
+        },
+        {
+          description: 'No significant depression',
+          points: 3,
+        },
+        {
+          description: 'Taking meds or therapy if applicable',
+          points: 1,
+        },
+        {
+          description: 'Feel content or happy most of the time',
+          points: 3,
+        },
+        {
+          description: 'Feel love for someone, family included',
+          points: 2,
+        },
+        {
+          description: "Think you're a nice person",
+          points: 1,
+        },
+        {
+          description: 'Not anxious around family and friends',
+          points: 3,
+        },
+        {
+          description: 'Not uncomfortable around strangers',
+          points: 1,
+        },
+        {
+          description: "Don't feel anxious about going outside",
+          points: 2,
+        },
+        {
+          description: "Don't feel stared at when outside",
+          points: 1,
+        },
+        {
+          description: 'Feel motivation during the day',
+          points: 2,
+        },
+        {
+          description: 'No wild changes in emotion',
+          points: 1,
+        },
+        {
+          description: 'Have a clean conscience',
+          points: 2,
+        },
+        {
+          description: "Don't find joy in shocking/gory material",
+          points: 1,
+        },
+        {
+          description: 'Get to sleep quite easity at night',
+          points: 1,
+        },
+        {
+          description: 'Get up quite easily in the morning',
+          points: 1,
+        },
+        {
+          description: 'No sexual attraction to under 15 year olds',
+          points: 3,
+        },
+      ],
+    },
+    {
+      name: 'Social',
+      color: '#388e3c',
+      criterias: [
+        {
+          description: 'Had at least one romantic partner before',
+          points: 5,
+        },
+        {
+          description: 'Had multiple romantic partners before',
+          points: 3,
+        },
+        {
+          description: 'Not a virgin',
+          points: 8,
+        },
+        {
+          description: 'Had sex with multiple partners before',
+          points: 4,
+        },
+        {
+          description: 'Kissed romantically before',
+          points: 3,
+        },
+        {
+          description: 'Held hands romantically before',
+          points: 1,
+        },
+        {
+          description: 'Had a relationship in the last 6 months',
+          points: 2,
+        },
+        {
+          description: 'Have a good relationship with family',
+          points: 2,
+        },
+        {
+          description: 'Have at least one friend in real life',
+          points: 2,
+        },
+        {
+          description: 'Have at least several friends in real life',
+          points: 4,
+        },
+        {
+          description: 'Socialise at least once a week',
+          points: 1,
+        },
+        {
+          description: 'Socialise at least once a day',
+          points: 3,
+        },
+        {
+          description: 'Leave the house on the regular',
+          points: 1,
+        },
+        {
+          description: 'Enjoy being around others on occasion',
+          points: 1,
+        },
+        {
+          description: 'Enjoy being around others when possible',
+          points: 4,
+        },
+        {
+          description: 'Recreational screen-time less than 4h a day',
+          points: 1,
+        },
+        {
+          description: 'Use social networks regularly',
+          points: 2,
+        },
+      ],
+    },
+    {
+      name: 'Accomplishments',
+      color: '#7b1fa2',
+      criterias: [
+        {
+          description: "Don't live with your parents",
+          points: 3,
+        },
+        {
+          description: 'Own a vehicle',
+          points: 1,
+        },
+        {
+          description: 'In education, employment or training',
+          points: 4,
+        },
+        {
+          description: "Have a hobby or sport which isn't video games",
+          points: 2,
+        },
+        {
+          description: 'Have a productive talent (cooking for example)',
+          points: 2,
+        },
+        {
+          description: 'Own or rent a living space',
+          points: 1,
+        },
+        {
+          description: 'Keep your living space clean',
+          points: 1,
+        },
+        {
+          description: 'Moving forward in life',
+          points: 2,
+        },
+        {
+          description: 'Have material items to be proud of',
+          points: 2,
+        },
+        {
+          description: 'Won an award to be proud of',
+          points: 2,
+        },
+        {
+          description: "Have children you're proud of",
+          points: 1,
+        },
+        {
+          description: 'Travelling or vacation in the past year',
+          points: 1,
+        },
+        {
+          description: 'Manage your money well',
+          points: 1,
+        },
+        {
+          description: 'Live a largely independent life',
+          points: 2,
+        },
+        {
+          description: 'Take care of a pet',
+          points: 1,
+        },
+        {
+          description: 'Attend or attended higher education',
+          points: 3,
+        },
+        {
+          description: 'Have a college degree',
+          points: 3,
+        },
+      ],
+    },
+    {
+      name: 'Bonus',
+      color: '#ef6c00',
+      criterias: [
+        {
+          description: 'You are female',
+          points: 6,
+        },
+        {
+          description: 'Have plenty of disposable income',
+          points: 1,
+        },
+        {
+          description: 'Have a very large number of contacts',
+          points: 3,
+        },
+        {
+          description: 'Have little interest in anime',
+          points: 2,
+        },
+        {
+          description: 'Have no interest in tabletop gaming',
+          points: 1,
+        },
+        {
+          description: 'Gaming is mostly limited to console',
+          points: 2,
+        },
+        {
+          description: 'Healthy view of the opposite sex',
+          points: 3,
+        },
+        {
+          description: 'No criminal record as an adult',
+          points: 1,
+        },
+        {
+          description: 'Been texted by a real person recently',
+          points: 2,
+        },
+        {
+          description: 'Enjoy using YikYak or Snapchat',
+          points: 1,
+        },
+        {
+          description: 'Have a decent smartphone',
+          points: 3,
+        },
+        {
+          description: 'Fetishes: none, feet, BDSM, DP, etc.',
+          points: 1,
+        },
+        {
+          description: "Don't consume hentai, ecchi, etc.",
+          points: 3,
+        },
+      ],
+    },
+  ],
+  levels: [
+    {
+      title: 'Grand Wizard',
+      description:
+        'No longer a human, you are barely attached to the living world by a thin thread. The curse of autism eternal and spaghetti all-powerful has consumed your life and soul as you straddle the line between this world and the next.',
+      minPoints: 0,
+      maxPoints: 8,
+      picture: 'levels/grandwizard.jpg',
+    },
+    {
+      title: 'Wizard',
+      description:
+        'You have given up conventional human desired and needs in favour of magical powers, only obtainable by some of the most concentrated, powerful autism physically possible.',
+      minPoints: 9,
+      maxPoints: 24,
+      picture: 'levels/wizard.jpg',
+    },
+    {
+      title: 'Wizard Apprentice',
+      description:
+        'Although hope for you is bleak, it is not impossible. You have a great potential for magical powers but time may yet heal your soul into that of a mortal.',
+      minPoints: 25,
+      maxPoints: 34,
+      picture: 'levels/wizardapprentice.jpg',
+    },
+    {
+      title: 'Robot',
+      description:
+        'Your flesh has turned to metal from feels. A little magic flows through your enlightened veins, and you are a likely suspect for a mass shooting in the beta uprising. However, this would require going outside.',
+      minPoints: 35,
+      maxPoints: 58,
+      picture: 'levels/robot.jpg',
+    },
+    {
+      title: 'Cyborg',
+      description:
+        'You are a sorry amalgam of robot and regular human. You linger in two worlds at once, an outsider to both hte normies and the robots. As a result, you may be on your own with your feels.',
+      minPoints: 59,
+      maxPoints: 81,
+      picture: 'levels/cyborg.jpg',
+    },
+    {
+      title: 'Slightly Strange',
+      description:
+        'You have several qualities of a robot but participate largely in the normalfag world. You probably even hang out with normals, although they can sense you are not one of them so you struggle socially.',
+      minPoints: 82,
+      maxPoints: 94,
+      picture: 'levels/slightlystrange.jpg',
+    },
+    {
+      title: 'Normalfag',
+      description:
+        "You don't necessarily have the best genes or looks, but you find socialising easy and probably have had plenty of relationships before. You have a very typical personnality and set of interests, and need to get off this site.",
+      minPoints: 95,
+      maxPoints: 115,
+      picture: 'levels/normalfag.jpg',
+    },
+    {
+      title: 'Chad/Stacey',
+      description:
+        "You are a step above the average normie. Blessed with some fine genetics, you don't need to work very hard to get what you want. Finding a bf/gf is like child's play to you and even the normies get a little jealous.",
+      minPoints: 116,
+      maxPoints: 129,
+      picture: 'levels/chadstacey.jpg',
+    },
+    {
+      title: 'Successful',
+      description:
+        'Your looks, intelligence and charisma have helped you overcome struggles, leading you to a wealthy and comfortable point in your life. All thanks to a mix of luck and talent, you are a shining example of a human.',
+      minPoints: 130,
+      maxPoints: 172,
+      picture: 'levels/successful.jpg',
+    },
+  ],
+};
